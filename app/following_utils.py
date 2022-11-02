0126b17885f429d210016c0b4f4abd2108d893e1
Kenny <kburawudi@yahoo.com>
2022-11-02 10:18:56 +0100

    update dash

--- app/following_utils.py
+++ app/following_utils.py
@@ -3,7 +3,6 @@ import pandas as pd
 def preprocess_following(rows, places):
 
     df = pd.DataFrame(rows)
-    df = pd.concat((df, pd.json_normalize(df['public_metrics'])), axis = 1)
     df = df.join(places.set_index('place'), on = 'location', how = 'inner')
 
     df = df.groupby(['longitude','latitude'], as_index = False).sum(numeric_only=True)
